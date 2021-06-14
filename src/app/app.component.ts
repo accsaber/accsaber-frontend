025c384b8e0bc14ac41677d7d499efae1a64a6b7
Jules Bulteau <jules.bulteau@epitech.eu>
2021-06-14 22:35:36 +0200

    Uncomment staff login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
     { linkName: 'Leaderboard', linkPath: '/leaderboard' },
     { linkName: 'Ranked Maps', linkPath: '/ranked-maps' },
     // { linkName: 'Biweekly Challenge (soon)', linkPath: '/weekly-challenge', disable: true },
-    // { linkName: 'Staff Login (soon)', linkPath: '/staff-login', last: true },
+    { linkName: 'Staff Login', linkPath: '/staff-login', last: true },
   ];
   isBeta: boolean;
 
